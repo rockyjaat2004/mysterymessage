550a0a50571559b6a3cfadb01050bfff4395d947
Harshit Saxena <harshit214saxena@gmail.com>
2026-03-03 19:04:16 +0530

    feat: add Open Graph and Twitter metadata for improved social sharing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Mystery Message",
   description: "Where your identity remains a secret.",
+  metadataBase: new URL("https://mysterymessage-delta.vercel.app"),
+
+  openGraph: {
+    title: "Mystery Message",
+    description:
+      "Where your identity remains a secret. Share your thoughts without revealing who you are.",
+    type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Mystery Message",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@harshits_twt",
+    images: ["/og-image.png"],
+  },
 };
 
 export default function RootLayout({
